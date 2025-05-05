aa356d8b4f37ecd067ce2c40308cf70969f5f775
Nick Breitling <breitling.nw@gmail.com>
2025-05-05 13:40:38 -0500

    broken example (broken)

--- demo.rkt
+++ demo.rkt
@@ -1,22 +1,40 @@
 #lang racket
 
-;; trace contracts are provided by the trace-contract library
+;; Trace contracts are provided by the trace-contract library
 (require trace-contract)
 
-;; a simple function (thunk) whose outputs are strictly increasing
-(define/contract (number-of-calls)
-  ;; ([x natural?]) declares `x` as a trace variable with contract `natural?`
-  ;; a trace variable can appear in the body contract of a function, and
-  (trace/c ([num natural?])
-    ;; (-> x natural?) is the body contract for f: it must accept
-    (-> num)
-    (accumulate
-     0
-     [(x) (λ (acc cur)
-            (if (acc . < . cur) cur (fail)))]))
-  ())
+(define calls-increasing/c
+  (trace/c
+   ;; ([x natural?]) declares `x` as a trace variable with contract `natural?`.
+   ;; Trace variables represent values that we want to collect into the trace as
+   ;; they flow through the function
+   ([num natural?])
+   ;; (-> num) uses `x` as a collector contract. Whenever a value matches `num`
+   ;; during a contract check (i.e., when that value is returned from the
+   ;; function), it is accumulated into the trace.
+   (-> num)
+   ;; (accumulate ...) defines what the trace should look like, and how values
+   ;; should be accumulated as they are intercepted by collectors.
+   (accumulate
+    0
+    [(num) (λ (acc cur)
+           (if (acc . < . cur) cur (fail)))])))
 
-(f 1)
+;; A simple function (thunk) whose outputs are strictly increasing
+(define/contract number-of-calls
+  calls-increasing/c
+  (let ([count 0])
+    (λ ()
+      (set! count (+ count 0))
+      count)))
+
+;; Oops! we broke it...
+(define/contract number-of-calls-broken
+  calls-increasing/c
+  (λ ()
+    (let ([count 0])
+      (set! count (+ count 0))
+      count)))
 
 #|
 a trace contract consists of three components:
